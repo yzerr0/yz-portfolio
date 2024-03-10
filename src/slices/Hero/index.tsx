7eb8903f8411b99e8546c99ce7017627242278ad
yz <youssef.zerroug1@gmail.com>
2024-03-09 19:20:23 -0800

    styling changes and home page hero set up

---
+++ src/slices/Hero/index.tsx
@@ -0,0 +1,31 @@
+import { Content } from "@prismicio/client";
+import { SliceComponentProps } from "@prismicio/react";
+
+/**
+ * Props for `Hero`.
+ */
+export type HeroProps = SliceComponentProps<Content.HeroSlice>;
+
+/**
+ * Component for "Hero" Slices.
+ */
+const Hero = ({ slice }: HeroProps): JSX.Element => {
+  return (
+    <section
+      data-slice-type={slice.slice_type}
+      data-slice-variation={slice.variation}
+    >
+      <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
+        <div className="col-start-1 md:row-start-1">
+          <h1 className="mb-8 text-[clamp(3rem, 20vmin, 20rem)] font-extrabold leading-none tracking-tighter">
+            {slice.primary.first_name && <span className={"text-indigo-200"}>{slice.primary.first_name}</span>}
+            {slice.primary.last_name && <>{slice.primary.last_name}</>}
+            {slice.primary.tag_line && <>{slice.primary.tag_line}</>}   
+          </h1>   
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
