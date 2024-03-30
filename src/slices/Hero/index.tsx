ced172346558b86b5a3b5b1f61dd9efd2b982f3c
yz <youssef.zerroug1@gmail.com>
2024-03-30 13:39:28 -0700

    imported gsap and made animation for name

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from "react";
 
 import { Content, KeyTextField } from "@prismicio/client";
@@ -16,14 +18,25 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
   const component = React.useRef(null);
   React.useEffect(() => {
     let ctx = gsap.context(() => {
-      
+      const timeline = gsap.timeline();
+      timeline.fromTo(
+        ".name-animation",
+        { opacity: 0, x: -100, rotate: -10 },
+        { opacity: 1, x: 0, rotate: 0, stagger: 0.1 }
+      );
+      // or gsap.timeline().fromTo(...) 
+      timeline.fromTo(
+        ".tagline-animation",
+        { opacity: 0, y: -100, rotate: -10 },
+        { opacity: 1, y: 0, rotate: 0 }
+      )
     }, component);
     return () => ctx.revert();
   }, []);
 
   const renderLetters = (name: KeyTextField, key: string) => {
-    if(slice.primary.first_name === undefined || slice.primary.first_name === null) throw new Error("First name missing in Hero slice.");
-    const letters = slice.primary.first_name.split('');
+    if(!name) throw new Error("First name missing in Hero slice.");
+    const letters = name.split('');
     return letters.map((letter, index) => {
       return <span 
         key={index} 
@@ -36,13 +49,14 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
     <section
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
+      ref={component}
     >
       <div className="grid min-h-[70vh] grid-cols-1 md:grid-cols-2 items-center">
         <div className="col-start-1 md:row-start-1">
           <h1 className="mb-8 text-clamp font-extrabold leading-none tracking-tighter" 
           aria-label={slice.primary.first_name + " " + slice.primary.last_name}>
             {slice.primary.first_name && <span className={"block text-indigo-200"}>{renderLetters(slice.primary.first_name, "first")}</span>}
-            {slice.primary.last_name && <span className={"-mt-1 text-indigo-400"}>{renderLetters(slice.primary.last_name, "last")}</span>}
+            {slice.primary.last_name && <span className={"block -mt-1 text-indigo-400"}>{renderLetters(slice.primary.last_name, "last")}</span>}
           </h1>
             {slice.primary.tag_line && <span 
             className={"block bg-gradient-to-tr from-yellow-500 via-yellow-200 to-yellow-500 bg-clip-text text-2xl font-bold uppercase tracking-[.15em] text-transparent opacity-100 md:text-4xl"}
